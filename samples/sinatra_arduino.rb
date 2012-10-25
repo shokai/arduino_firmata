f81c36ec21225adb707769751cce6ad2e0435142
Sho Hashimoto <hashimoto@shokai.org>
2012-10-25 13:58:32 +0900

    fix sample

--- samples/sinatra_arduino.rb
+++ samples/sinatra_arduino.rb
@@ -13,11 +13,11 @@ end
 get '/on' do
   analog = arduino.analog_read(0)
   arduino.digital_write 13, ArduinoFirmata::HIGH
-  "<p>analog : #{analog}</p><p><a href='./off'>off</a></p>"
+  "<p>analog : #{analog}</p><p><a href='./off'>LED OFF</a></p>"
 end
 
 get '/off' do
   analog = arduino.analog_read(0)
   arduino.digital_write 13, ArduinoFirmata::LOW
-  "<p>analog : #{analog}</p><p><a href='./on'>on</a></p>"
+  "<p>analog : #{analog}</p><p><a href='./on'>LED ON</a></p>"
 end
