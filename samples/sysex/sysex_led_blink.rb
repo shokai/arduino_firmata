4c5b9a3a1b8f3536fc2c37c04e9ef3b2f929546b
Sho Hashimoto <hashimoto@shokai.org>
2013-01-11 19:40:40 +0900

    fix method name sysex_write -> send_sysex  #14

--- samples/sysex/sysex_led_blink.rb
+++ samples/sysex/sysex_led_blink.rb
@@ -6,5 +6,5 @@ require 'arduino_firmata'
 arduino = ArduinoFirmata.connect ARGV.shift
 puts "firmata version #{arduino.version}"
 
-arduino.sysex_write 0x01, [13, 5, 2]  # pin13, blink 5 times, 200 msec interval
-arduino.sysex_write 0x01, [11, 3, 10]  # pin11, blink 3 times, 1000 msec interval
+arduino.send_sysex 0x01, [13, 5, 2]  # pin13, blink 5 times, 200 msec interval
+arduino.send_sysex 0x01, [11, 3, 10]  # pin11, blink 3 times, 1000 msec interval
