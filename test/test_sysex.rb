0fc8ff16ee2ec33b56f0482df34731608f3dd298
Sho Hashimoto <hashimoto@shokai.org>
2013-03-13 17:18:15 +0900

    add sysex command test  #20

---
+++ test/test_sysex.rb
@@ -0,0 +1,34 @@
+## use samples/sysex/SysexLedBlinkFirmata/SysexLedBlinkFirmata.ino
+
+require File.expand_path 'test_helper', File.dirname(__FILE__)
+
+class TestSysex < MiniTest::Unit::TestCase
+
+  def setup
+    @arduino = ArduinoFirmata.connect ENV['ARDUINO']
+  end
+
+  def teardown
+    @arduino.close
+  end
+
+  def test_sysex_command
+    __cmd = nil
+    __data = nil
+    @arduino.on :sysex do |command, data|
+      __cmd = command
+      __data = data
+    end
+
+    @arduino.sysex 0x01, [13, 3, 2]
+
+    50.times do
+      sleep 0.1
+      break if __cmd != nil
+    end
+
+    assert __cmd == 0x01
+    assert __data == [13, 0, 3, 0, 2, 0]
+  end
+
+end
